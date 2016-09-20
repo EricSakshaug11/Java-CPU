19d41c32321ddb05f8a67cd3c3f4bae6d0fdeec9
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-09-20 18:26:13 -0400

    Added add and subtract to ALU

--- code/ALU.java
+++ code/ALU.java
@@ -1,11 +1,20 @@
 public class ALU{
 
+    private boolean carryBit;
+
     public ALU(){
 
     }
 
-    public Register add(Register inA, Register inB){
+    public Register32 add(Register32 inA, Register32 inB){
+	Register32 toReturn = new Register32();
+	toReturn.store(inA.read() + inB.read());
+	return toReturn;
+    }
 
+    public Register32 subtract(Register32 inA, Register32 inB){
+	Register toReturn = new Register32();
+	toReturn.store(inA.read(), inB.read());
     }
 
 }
